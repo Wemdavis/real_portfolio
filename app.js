b749ca17dac5aff6e53eb3f2b5bdce7e422b01ba
Wemdavis <fasunonbukola@gmail.com>
2023-03-31 21:38:22 +1300

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.cont');
+const allSections= document.querySelector('.main-cont');
+
+
+function pageTrans(){
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+          sectBtns.forEach((btn) => {
+            btn.classList.remove('active');
+          });
+          e.target.classList.add('active');
+          sections.forEach((section) => {
+            section.classList.remove('active');
+          });
+          const element = document.getElementById(id);
+          element.classList.add('active');
+        }
+      });
+}
+pageTrans();
